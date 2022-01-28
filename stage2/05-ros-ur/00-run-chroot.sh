b7f0c77d5685aa3472ac728e036c293b794faca6
Michael Terzer <michael.terzer@fraunhofer.it>
2022-01-29 00:44:17 +0100

    wip

--- stage2/05-ros-ur/00-run-chroot.sh
+++ stage2/05-ros-ur/00-run-chroot.sh
@@ -30,12 +30,11 @@ install_sistem_deps(){
 #    wget -O /etc/ros/rosdep/sources.list.d/20-default.list https://raw.githubusercontent.com/ros/rosdistro/master/rosdep/sources.list.d/20-default.list || echo 'wget failt to get list -------------------------'
     export SSL_CERT_FILE=/usr/lib/ssl/certs/ca-certificates.crt
     [ -f /etc/ros/rosdep/sources.list.d/20-default.list ] && rm /etc/ros/rosdep/sources.list.d/20-default.list
-    rosdep init || echo 'rosdep init failed ----' && exit 1
+    rosdep init || echo 'rosdep init failed ----' #&& exit 1
 #    rosdep fix-permissions
 #    #rosdep init || echo 'rosdep init failed'
 #    echo '-------- rosdep update -----'
-    rosdep update || echo 'rosdep update failed' && exit 1
-echo hola
+    rosdep update || echo 'rosdep update failed' #&& exit 1
 }
 
 install_ros_base(){
@@ -45,6 +44,8 @@ install_ros_base(){
     mkdir -p /ros_ws/src
     cd /ros_ws
     # 1) generated data to install ros
+    [ -f ros.rosinstall ] && rm ros.rosinstall
+    [ -f src/.rosinstall ] && rm src/.rosinstall
     rosinstall_generator controller_manager_msgs roscpp std_msgs controller_interface hardware_interface joint_trajectory_controller pluginlib realtime_tools actionlib_msgs message_generation actionlib control_msgs controller_manager geometry_msgs industrial_robot_status_interface sensor_msgs std_srvs tf tf2_geometry_msgs tf2_msgs trajectory_msgs robot_state_publisher joint_state_publisher map_msgs position_controllers tf_conversions joint_state_controller velocity_controllers force_torque_sensor_controller --rosdistro noetic --deps --wet-only --tar > ros.rosinstall
     # 2) use wstool to download the sources
     wstool init -j8 src ros.rosinstall
@@ -63,7 +64,19 @@ install_ros_base(){
     rosdep install -r -q  --from-paths src --ignore-src --rosdistro noetic -y
 
     # 4) compile with cmake
-    ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j2
+    #./src/catkin/bin/catkin_make_isolated --install \
+    cat /usr/share/cmake-3.16/Modules/CMakeDetermineCompilerId.cmake
+    catkin config \
+     --install \
+        -DCMAKE_BUILD_TYPE=Release \
+        --install-space /opt/ros/noetic -j2 \
+        -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+        -DCATKIN_SKIP_TESTING=ON
+
+    catkin config
+    catkin env
+    catkin clean -y
+    catkin build
 
     # 5) append enviroment variables to bashrc
 }
